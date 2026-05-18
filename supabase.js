c6e6292fef86b197f12d3d28b0c92b52a24c24ae
duddev <eduardoalves.dev2024@gmail.com>
2026-05-17 22:23:16 -0300

    BD JS

---
+++ supabase.js
@@ -0,0 +1,7 @@
+
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm';
+
+export const sb = createClient(
+  'https://SEU_URL.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZnb3FiYXhyYWdra2Zxbm16bHJxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg4OTcyMTksImV4cCI6MjA5NDQ3MzIxOX0.mSA5vNxzRZefNJMptTJiEIlDIDXxA25h7QPtXd01_ao'
+);
